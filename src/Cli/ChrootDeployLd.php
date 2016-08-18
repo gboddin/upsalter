5e0411924e33d5a8f263a47aad3802c5a9bb0bf0
Gregory Boddin <gregory@siwhine.net>
2016-08-18 17:11:29 +0200

    Finished LD_* deploy system for separate users

--- src/Cli/ChrootDeployLd.php
+++ src/Cli/ChrootDeployLd.php
@@ -18,18 +18,76 @@ class ChrootDeployLd extends Command
             ->setName('chroot:create-ld-user')
             // the short description shown while running "php bin/console list"
             ->setDescription('Creates ld-user on server and register cron')
-            ->addArgument('destination', InputArgument::REQUIRED, 'Server to deploy to (user@server:containerdirectory)')
+            ->addArgument('user', InputArgument::REQUIRED, 'Remote user')
+            ->addArgument('server', InputArgument::REQUIRED, 'Server to deploy to')
+            ->addArgument('location', InputArgument::REQUIRED, 'Remote container directory')
             ->addOption('skip-start','S',InputOption::VALUE_NONE,'Skip startup of minion (implies -R)');
     }
 
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        if(!file_exists($input->getArgument('package'))) {
-            throw new \Exception('Package not found');
-        }
-        exec('ssh -oBatchMode=yes /bin/true',$output,$rc);
+        $user = $input->getArgument('user');
+        $server = $input->getArgument('server');
+        $location = $input->getArgument('location');
+        $skipStart = $input->getArgument('skip-start');
+
+        $cmd = '[ -x '.escapeshellarg($location.DIRECTORY_SEPARATOR.'manage').' ]';
+        exec('ssh -l '.escapeshellarg($user).' '.escapeshellarg($server).' -oBatchMode=yes '.escapeshellarg($cmd),$cmdOutput,$rc);
         if($rc > 0) {
-            throw new\Exception('Connection to ');
+            throw new\Exception('SSH connection to '.$server.' failed!');
         }
+        $output->writeln('<info>Access to '.$server.' granted and working</info>');
+
+        $cmd = '[ ! -d '.escapeshellarg($location.DIRECTORY_SEPARATOR.'etc/supervisor-'.$user).' ] \\
+           && mkdir -p '.escapeshellarg($location.DIRECTORY_SEPARATOR.'etc/supervisor-'.$user.'/conf.d');
+
+        exec('ssh -l '.escapeshellarg($user).' '.escapeshellarg($server).' -oBatchMode=yes '.escapeshellarg($cmd),$cmdOutput,$rc);
+        if($rc > 0) {
+            throw new\Exception('SSH connection to '.$server.' failed!');
+        }
+        $output->writeln('<info>lduser '.$user.' not implemented yet, working ...</info>');
+
+        $cmd = 'scp '.escapeshellarg(APP_DIR.DIRECTORY_SEPARATOR.'resources'.DIRECTORY_SEPARATOR.'supervisor-user.conf').' '.escapeshellarg(
+                $user.'@'.$server.':'.$location.DIRECTORY_SEPARATOR.'etc/supervisor-'.$user.'/supervisor.conf');
+        exec($cmd,$cmd,$rc);
+        if($rc > 0) {
+            throw new\Exception('Supervisor user config : '.$server.' deploy failed');
+        }
+
+        $cmd = 'sed -i "s/__ROOT__/'.str_replace("/","\/",$location).'/g" '.escapeshellarg($location.DIRECTORY_SEPARATOR.'etc/supervisor-'.$user.'/supervisor.conf');
+        $cmd = 'ssh -l '.escapeshellarg($user).' '.escapeshellarg($server).' -oBatchMode=yes '.escapeshellarg($cmd);
+        exec($cmd,$cmd,$rc);
+        if($rc > 0) {
+            throw new\Exception('Supervisor user config for  '.$server.' - deploy failed');
+        }
+
+        $cmd = 'sed -i "s/__USER__/'.str_replace("/","\/",$user).'/g" '.escapeshellarg($location.DIRECTORY_SEPARATOR.'etc/supervisor-'.$user.'/supervisor.conf');
+        $cmd = 'ssh -l '.escapeshellarg($user).' '.escapeshellarg($server).' -oBatchMode=yes '.escapeshellarg($cmd);
+        exec($cmd,$cmd,$rc);
+        if($rc > 0) {
+            throw new\Exception('Supervisor user config for '.$server.' - deploy failed');
+        }
+
+        $cmd = '[ ! -d '.escapeshellarg($location.DIRECTORY_SEPARATOR.'var/log/supervisor-'.$user).' ] \\
+           && mkdir -p '.escapeshellarg($location.DIRECTORY_SEPARATOR.'var/log/supervisor-'.$user);
+
+        exec('ssh -l '.escapeshellarg($user).' '.escapeshellarg($server).' -oBatchMode=yes '.escapeshellarg($cmd),$cmdOutput,$rc);
+        if($rc > 0) {
+            throw new\Exception('Supervisor user config for'.$server.' failed!');
+        }
+
+        $output->writeln('<info>User '.$user.' setup and ready!</info>');
+
+        if(!$skipStart) {
+            $cmd = escapeshellarg($location.'/manage').' start-user';
+            $cmd = 'ssh -l '.escapeshellarg($user).' '.escapeshellarg($server).' -oBatchMode=yes '.escapeshellarg($cmd);
+            exec($cmd,$cmd,$rc);
+            if($rc > 0) {
+                throw new\Exception('Error starting '.$user.' on '.$server);
+            }
+            $output->writeln('<info>User '.$user.' started !</info>');
+
+        }
+
     }
 }
