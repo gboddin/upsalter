b5ef3e6093869a17877736d238008ea55a5e8fba
Gregory Boddin <gregory@siwhine.net>
2016-09-12 12:25:01 +0200

    Fix proot run issue

--- resources/manage.sh
+++ resources/manage.sh
@@ -5,7 +5,7 @@ PROOT_ARGS=""
 
 cd ${MY_DIR}
 
-[ -f proot.cfg ] && source proot.cfg
+[ -f ${MY_DIR}/proot.cfg ] && source ${MY_DIR}/proot.cfg
 
 my_ld_library_path="${MY_DIR}/lib64:${MY_DIR}/usr/lib64"
 
@@ -15,22 +15,22 @@ done < <(cat "${MY_DIR}/etc/ld.so.conf.d"/* 2> /dev/null | grep -v '^#')
 
 my_path=${MY_DIR}/bin:${MY_DIR}/sbin:${MY_DIR}/usr/bin:${MY_DIR}/usr/sbin:${MY_DIR}/usr/local/bin:${MY_DIR}/usr/local/bin:
 
-proot_run="HOME=/root SHELL=/bin/bash PATH=/bin:/sbin:/usr/sbin:/usr/bin ./proot ${PROOT_ARGS} -w /root -b ${MY_DIR} -b /etc/mtab -b /etc/resolv.conf -b /etc/hostname -b /dev -b /sys -b /proc -r ${MY_DIR} -0"
+proot_final_args="${PROOT_ARGS} -w /root -b ${MY_DIR} -b /etc/mtab -b /etc/resolv.conf -b /etc/hostname -b /dev -b /sys -b /proc -r ${MY_DIR} -0"
 
 if [ "$1" = "start-minion" ]; then
         echo "Starting root minion supervisor ..."
-        $proot_run /usr/bin/supervisord -c /etc/supervisor/supervisord.conf > proot.log 2> proot.err &
+        ./proot ${proot_final_args} /usr/bin/supervisord -c /etc/supervisor/supervisord.conf > proot.log 2> proot.err &
 fi
 
 if [ "$1" = "stop-minion" ]; then
         echo "Stopping root minion supervisor ..."
-        $proot_run supervisorctl shutdown
+        ./proot ${proot_final_args} supervisorctl shutdown
 fi
 
 if [ "$1" = "admin" ]; then
         echo "Dropping admin shell ..."
         shift
-        $proot_run "$@"
+        ./proot ${proot_final_args} "$@"
 fi
 
 if [ "$1" = "start-user" ]; then
