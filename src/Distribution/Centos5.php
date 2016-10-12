83fe8bfab6808641a0e1376adc3437bd9d45a838
Gregory Boddin <gregory@siwhine.net>
2016-10-12 13:21:34 +0200

    Trying support for el5 on upsalter

---
+++ src/Distribution/Centos5.php
@@ -0,0 +1,63 @@
+<?php
+namespace Upsalter\Distribution {
+
+    use Upsalter\BaseDistribution;
+
+    class Centos5 extends BaseDistribution
+    {
+
+        protected $epelEnabled = false;
+
+        public function installSaltMinion()
+        {
+            $this->enableEpel();
+            $this->prootRun('rpm -Uvh https://repo.saltstack.com/yum/redhat/salt-repo-latest-1.el5.noarch.rpm');
+            $this->prootRun('yum -y install salt-minion');
+        }
+
+        public function installSupervisor()
+        {
+            $this->enableEpel();
+            $this->prootRun('yum install python-pip -y');
+            $this->prootRun('easy_install --upgrade supervisor');
+            //enabling debian directory structure :
+            $this->prootRun('mkdir -p /etc/supervisor/conf.d');
+            $this->prootRun('mkdir -p /var/log/supervisor');
+        }
+
+        public function enableEpel()
+        {
+            if(!$this->epelEnabled)
+                $this->prootRun('rpm -Uvh https://dl.fedoraproject.org/pub/epel/epel-release-latest-5.noarch.rpm');
+            $this->epelEnabled = true;
+        }
+
+        public function init() {
+
+        }
+
+        public function clean() {
+            $this->prootRun('yum clean all');
+        }
+
+        public function getRootFsUrl()
+        {
+            return 'https://github.com/gboddin/linux-rootfs/releases/download/latest/centos-5.11.tar.bz2';
+        }
+
+        public function getAliases()
+        {
+            return array(
+                'centos'
+            );
+        }
+
+        public function getVersions()
+        {
+            return array(
+              5
+            );
+        }
+
+    }
+}
